fcef8e95e33f4ec4dd635278c545dd3a7b9ba2b7
Dillon Beliveau <dillonbeliveau@gmail.com>
2024-08-06 01:08:02 -0700

    Add Nix flake with a devshell

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (system: {
+        default = pkgs.${system}.mkShell {
+          buildInputs = with pkgs.${system}; [ cargo ];
+        };
+      });
+    };
+}
